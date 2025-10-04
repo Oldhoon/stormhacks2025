a6582e6264295d106d29bdf2336d8620edf8f3e9
benjamin-am <bleblanc1904@gmail.com>
2025-10-04 13:04:04 -0700

    set up background art, and implemented it in pygame

--- arena.py
+++ arena.py
@@ -9,9 +9,17 @@ FPS.tick(60)
 
 pygame.init()
 
-#test smth
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-DISPLAYSURF = pygame.display.set_mode((300,300))
+# Clock for frame rate
+clock = pygame.time.Clock()
+FPS = 60
+
+#test smth
+background = pygame.image.load(r"assets/cyberpunk-street-files/cyberpunk-street-files/Assets/Version 1/PNG/cyberpunk-street.png").convert()
+background = pygame.transform.scale(background, (SCREEN_WIDTH, SCREEN_HEIGHT))
 # Game loop begins
 while True:
     #TODO
@@ -20,6 +28,12 @@ while True:
             pygame.quit()
             sys.exit()
     pygame.display.update()
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+    clock.tick(FPS)
+    
+pygame.quit()
+sys.exit()
 
 
 
